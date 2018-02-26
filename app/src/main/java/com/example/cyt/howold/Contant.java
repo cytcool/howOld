b27b1d5d5a5b11fb69ba6955a21441e3f506a7e7
cytcool <675149948@qq.com>
2018-02-26 23:00:44 +0800

    初始化

---
+++ app/src/main/java/com/example/cyt/howold/Contant.java
@@ -0,0 +1,11 @@
+package com.example.cyt.howold;
+
+/**
+ * Created by CYT on 2018/2/26.
+ */
+
+public class Contant {
+
+    public static final String KEY = "y_hxxQQ32t4NdaCfOwpt5TEc0l52tjQ9";
+    public static final String SECRET = "1eZqq--ypHltyzTGN78EgJpjb6aWiYbV";
+}
