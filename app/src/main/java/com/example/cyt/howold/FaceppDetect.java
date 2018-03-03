118984e4aa1b053f79e3350a7e462c9dcaeb2d0f
cytcool <675149948@qq.com>
2018-03-03 21:58:45 +0800

    detect工具类完善

--- app/src/main/java/com/example/cyt/howold/FaceppDetect.java
+++ app/src/main/java/com/example/cyt/howold/FaceppDetect.java
@@ -6,9 +6,12 @@ import android.provider.SyncStateContract;
 
 import com.facepp.error.FaceppParseException;
 import com.facepp.http.HttpRequests;
+import com.facepp.http.PostParameters;
 
 import org.apache.http.HttpRequest;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by CYT on 2018/3/2.
@@ -28,9 +31,31 @@ public class FaceppDetect {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpRequest request = (HttpRequest) new HttpRequests(Contant.KEY,Contant.SECRET,true,true);
+
+                try {
+                    HttpRequests request = (HttpRequests) new HttpRequests(Contant.KEY,Contant.SECRET,true,true);
+                    Bitmap bmSmall = Bitmap.createBitmap(bm,0,0,bm.getWidth(),bm.getHeight());
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bmSmall.compress(Bitmap.CompressFormat.JPEG,100,stream);
+
+                    byte[] arrays = stream.toByteArray();
+
+                    PostParameters parameters = new PostParameters();
+                    parameters.setImg(arrays);
+                    JSONObject jsonObject = request.detectionDetect(parameters);
+
+                    if (callBack != null){
+                        callBack.success(jsonObject);
+                    }
+
+                } catch (FaceppParseException e) {
+                    e.printStackTrace();
+                    if (callBack != null){
+                        callBack.error(e);
+                    }
+                }
             }
-        });
+        }).start();
 
     }
 }
